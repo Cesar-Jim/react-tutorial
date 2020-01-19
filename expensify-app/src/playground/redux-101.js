e4af55348e021b4ce0ad97ebd437acc2b1d3caac
Cesar Jimenez <cijgr@hotmail.com>
2020-01-19 15:20:52 -0600

    ready to begin combining reducers

--- expensify-app/src/playground/redux-101.js
+++ expensify-app/src/playground/redux-101.js
@@ -1,6 +1,29 @@
 import { createStore } from 'redux';
 
-const store = createStore((state = { count: 0 }, action) => {
+// Action generators
+const incrementCount = ({ incrementBy = 1 } = {}) => ({
+  type: 'INCREMENT',
+  incrementBy
+});
+
+const decrementCount = ({ decrementBy = 1 } = {}) => ({
+  type: 'DECREMENT',
+  decrementBy
+});
+
+const setCount = ({ count }) => ({
+  type: 'SET',
+  count
+});
+
+const resetCount = () => ({
+  type: 'RESET'
+});
+
+// Reducers
+// 1. Reducers are pure functions: The output is only determined by the input. It doesn't use anything outside of it's own scope
+// 2. Never change state or action
+const countReducer = (state = { count: 0 }, action) => {
   switch (action.type) {
     case 'INCREMENT':
       const incrementBy =
@@ -8,56 +31,47 @@ const store = createStore((state = { count: 0 }, action) => {
       return {
         count: state.count + incrementBy
       };
+
     case 'DECREMENT':
       const decrementBy =
         typeof action.decrementBy === 'number' ? action.decrementBy : 1;
       return {
         count: state.count - decrementBy
       };
+
     case 'SET':
       return {
         count: action.count
       };
+
     case 'RESET':
       return {
         count: 0
       };
+
     default:
       return state;
   }
-});
+};
+
+const store = createStore(countReducer);
 
 const unsubscribe = store.subscribe(() => {
-  // subscribe serves to 'do something' when the state changes
   console.log(store.getState());
 });
 
-// Actions - are objects that gets sent to the store
+// Actions are objects that get sent to the store
+// store.dispatch({
+//   type: 'INCREMENT',
+//   incrementBy: 5
+// });
 
-// Increment the count
-store.dispatch({
-  type: 'INCREMENT',
-  incrementBy: 5
-});
+store.dispatch(incrementCount({ incrementBy: 5 }));
+store.dispatch(incrementCount());
 
-store.dispatch({
-  type: 'INCREMENT'
-});
+store.dispatch(resetCount());
 
-store.dispatch({
-  type: 'RESET'
-});
+store.dispatch(decrementCount());
+store.dispatch(decrementCount({ decrementBy: 10 }));
 
-store.dispatch({
-  type: 'DECREMENT'
-});
-
-store.dispatch({
-  type: 'DECREMENT',
-  decrementBy: 10
-});
-
-store.dispatch({
-  type: 'SET',
-  count: 101
-});
+store.dispatch(setCount({ count: -50 }));
